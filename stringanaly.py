2a762e27708c767934c4284675fea031d53b5b94
Imran Nazir <imanimrannazir1807@gmail.com>
2026-07-12 04:54:51 +0530

    Add string fundamentals and slicing practice

---
+++ stringanaly.py
@@ -0,0 +1,9 @@
+def main():
+    str = "artificial intelligence"
+    index = str.find(" ")
+    print("Index of first space:", index)
+    print(str[0:index])
+    print(str[index + 1:])
+if __name__ == "__main__":
+    main()
+    
